faff62f7e572cb49aa72c729e0149aea44641d46
Vince Abbate <vinceabbate@Vinces-MacBook-Pro.local>
2023-10-13 20:21:28 -0700

    Adding project to repo

--- client/src/components/h2Heading.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const H2Heading = ({ text }) => {
-    return <h2>{text}</h2>;
-};
-
-export default H2Heading;
--- server/flask_app/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from flask_bcrypt import Bcrypt
-from flask_cors import CORS
-
-app = Flask(__name__)
-CORS(app)
-bcrypt=Bcrypt(app)
-app.secret_key = 'solo project'
